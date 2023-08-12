2e477930b4debef75a3be54710b6a4f4528af0ce
AmareshKumarPanigrahi <amareshkumaepanigrahi@gmail.com>
2023-08-12 20:00:21 +0530

    p

---
+++ Practice/src/test/java/com/Test/AmazonTest.java
@@ -0,0 +1,15 @@
+package com.Test;
+
+import org.testng.annotations.Test;
+
+public class AmazonTest {
+
+	@Test
+	public void findingElementIphone14() {
+		
+		//div[contains(@data-asin,'PDX') and @data-component-id="4"]//span[contains(text(),'Apple iPhone')]/../../../following-sibling::div[@class='sg-row']//span[@class='a-price']/span[@class='a-offscreen']
+		
+		
+		
+	}
+}
